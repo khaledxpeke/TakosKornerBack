a6ab5c55b32dff33625ff78965a3520445f21262
khaled bouajila <khaledbouajila5481@gmail.com>
2023-06-06 18:25:49 +0100

    update add product function

--- controllers/productController.js
+++ controllers/productController.js
@@ -34,7 +34,6 @@ exports.addProductToCategory = async (req, res, next) => {
     const name = req.body.name.replace(/"/g, "");
     const image = req.file.path; // Get the image file path from the request
     const { currency, type, maxIngrediant } = req.body;
-    const typeIds = type?.split(",") || [];
     const ingrediantIds = req.body.ingrediants?.split(",") || [];
     const supplementIds = req.body.supplements?.split(",") || [];
     try {
@@ -50,7 +49,7 @@ exports.addProductToCategory = async (req, res, next) => {
           price,
           category: categoryId,
           currency,
-          type: typeIds,
+          type: [],
           createdBy: userId,
           maxIngrediant,
           ingrediants: ingrediantIds,
@@ -60,6 +59,24 @@ exports.addProductToCategory = async (req, res, next) => {
           product.image = image;
           await product.save();
         }
+        const productIngrediants = await Promise.all(
+          ingrediantIds.map(async (ingrediant) => {
+            return await Ingrediant.findById(ingrediant);
+          })
+        );
+        const types = productIngrediants.map((ingrediant) => ingrediant.type);
+        const uniqueTypes = types.reduce((unique, current) => {
+          const isDuplicate = unique.some(
+            (obj) => obj.valueOf() === current.valueOf()
+          );
+          if (!isDuplicate) {
+            unique.push(current);
+          }
+          return unique;
+        }, []);
+        product.type = uniqueTypes;
+
+        // Save the product
         const savedProduct = await product.save();
 
         const updatedCategory = await Category.findByIdAndUpdate(
@@ -67,7 +84,10 @@ exports.addProductToCategory = async (req, res, next) => {
           { $push: { products: savedProduct._id } },
           { new: true }
         );
-
+        await Product.updateMany(
+          { type: { $in: uniqueTypes } },
+          { $push: { product: savedProduct._id } }
+        );
         await Ingrediant.updateMany(
           { _id: { $in: ingrediantIds } },
           { $push: { product: savedProduct._id } }
