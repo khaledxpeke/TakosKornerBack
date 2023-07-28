c4db29aaa92d614a0fe2fd9bdd3516c6a393f7f0
khaled bouajila <khaledbouajila5481@gmail.com>
2023-07-28 12:01:19 +0100

    update delete type

--- controllers/typeController.js
+++ controllers/typeController.js
@@ -1,10 +1,14 @@
 const Type = require("../models/type");
+const Ingrediant = require("../models/ingrediant");
+const Product = require("../models/product");
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const jwtSecret = process.env.JWT_SECRET;
 app.use(express.json());
+const fs = require('fs');
+const path = require('path');
 
 exports.createType = async (req, res, next) => {
   const { name,message,free ,quantity} = req.body;
@@ -70,7 +74,26 @@ exports.updateType = async (req, res, next) => {
 exports.deleteType = async (req, res, next) => {
   try {
     const { typeId } = req.params;
+    const type = await Type.findById(typeId);
+    if (!type) {
+      return res.status(404).json({ message: "Type not found" });
+    }
+
+    const ingredients = await Ingrediant.find({ type: typeId });
+    for (const ingredient of ingredients) {
+
+      if (ingredient.image) {
+        const imagePath = path.join(__dirname, '..', ingredient.image);
+        fs.unlinkSync(imagePath);
+      }
+    }
     await Type.findByIdAndDelete(typeId);
+    await Ingrediant.deleteMany({ type: typeId });
+
+    await Product.updateMany(
+      { type: typeId },
+      { $pull: { type: typeId } }
+    );
     res.status(200).json({ message: "Type supprimer avec succées" });
   } catch (error) {
     res.status(400).json({
